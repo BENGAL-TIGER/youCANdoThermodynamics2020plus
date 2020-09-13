dbc9fed039a93f8eab502eff589aaf2184cb38a4
mdAshford <marcus@psydat.com>
2020-09-13 17:06:22 -0500

    put extensions first in custom.js

--- binder/custom/custom.js
+++ binder/custom/custom.js
@@ -1,31 +1,26 @@
 // MathJax extensioons
 
 MathJax.Hub.Config({
-  tex2jax: {
-      inlineMath: [["$", "$"], ["\\(", "\\)"]],
-      displayMath: [["$$", "$$"], ["\\[", "\\]"]],
-      processEscapes: true,
-  },
   TeX: {
-      // AutoNumbering of displayed formulas
-      equationNumbers: { autoNumber: "AMS" },
+    extensions: [
+      "color.js", // Color support in LaTeX
+      "autobold.js", // support for \boldsymbol{...}
+      "AMSmath.js",
+      "AMSsymbols.js",
+      "AMScd.js", // http://www.jmilne.org/not/Mamscd.pdf
+      "bbox.js", // support for \bbox[options]{math}
+      "begingroup.js", // mainly for formulas localization
+      "cancel.js", // support for strikethrough formulas
+      "HTML.js", // works by default
+      "mhchem.js", // chemical formulas
+      "unicode.js" // works by default
+  ],
+  // AutoNumbering of displayed formulas
+      // equationNumbers: { autoNumber: "AMS" },
       // All mathjax extensions:
       // http://docs.mathjax.org/en/latest/tex.html#tex-and-latex-extensions
       // Source code for all extensions:
       // https://github.com/mathjax/MathJax/tree/master/extensions/TeX
-      extensions: [
-          "color.js", // Color support in LaTeX
-          "autobold.js", // support for \boldsymbol{...}
-          "AMSmath.js",
-          "AMSsymbols.js",
-          "AMScd.js", // http://www.jmilne.org/not/Mamscd.pdf
-          "bbox.js", // support for \bbox[options]{math}
-          "begingroup.js", // mainly for formulas localization
-          "cancel.js", // support for strikethrough formulas
-          "HTML.js", // works by default
-          "mhchem.js", // chemical formulas
-          "unicode.js" // works by default
-      ]
   }
 });
 
