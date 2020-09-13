dc4334635d91ef1eb8de67ef4bc25d33353b3868
mdAshford <marcus@psydat.com>
2020-09-13 17:50:28 -0500

    forgot to mod gitignore for .jupyter

---
+++ binder/.jupyter/custom/custom.js
@@ -0,0 +1,39 @@
+// MathJax extensioons
+
+MathJax.Hub.Config({
+  TeX: {
+    extensions: [
+      "color.js", // Color support in LaTeX
+      "autobold.js", // support for \boldsymbol{...}
+      "AMSmath.js",
+      "AMSsymbols.js",
+      "AMScd.js", // http://www.jmilne.org/not/Mamscd.pdf
+      "bbox.js", // support for \bbox[options]{math}
+      "begingroup.js", // mainly for formulas localization
+      "cancel.js", // support for strikethrough formulas
+      "HTML.js", // works by default
+      "mhchem.js", // chemical formulas
+      "unicode.js" // works by default
+    ],
+    tex2jax: {
+      inlineMath: [["$", "$"], ["\\(", "\\)"]],
+      displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+      processEscapes: true,
+    },
+  // AutoNumbering of displayed formulas
+      // equationNumbers: { autoNumber: "AMS" },
+      // All mathjax extensions:
+      // http://docs.mathjax.org/en/latest/tex.html#tex-and-latex-extensions
+      // Source code for all extensions:
+      // https://github.com/mathjax/MathJax/tree/master/extensions/TeX
+  }
+});
+
+MathJax.Hub.Queue(
+  // ["resetEquationNumbers", MathJax.InputJax.TeX],
+  ["PreProcess", MathJax.Hub],
+  ["Reprocess", MathJax.Hub]
+ );
+
+// http://docs.mathjax.org/en/latest/configuration.html#configuring-mathjax-after-it-is-loaded
+MathJax.Hub.Configured()
